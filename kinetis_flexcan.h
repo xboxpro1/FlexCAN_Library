76987ef57991dc18614461d3629924aee617f600
Martin <xboxpro1@gmail.com>
2016-06-08 20:32:03 +0200

    Update kinetis_flexcan.h

--- kinetis_flexcan.h
+++ kinetis_flexcan.h
@@ -9,24 +9,20 @@
 #include <stdint.h>
 
 /* Bus speed definitions */
-#define CAN_5KBPS    1
-#define CAN_10KBPS   2
-#define CAN_20KBPS   3
-#define CAN_25KBPS   4 
-#define CAN_31K25BPS 5
-#define CAN_33KBPS   6
-#define CAN_40KBPS   7
-#define CAN_50KBPS   8
-#define CAN_80KBPS   9
-#define CAN_83K3BPS  10
-#define CAN_95KBPS   11
-#define CAN_100KBPS  12
-#define CAN_125KBPS  13
-#define CAN_200KBPS  14
-#define CAN_250KBPS  15
-#define CAN_500KBPS  16
-#define CAN_666KBPS  17
-#define CAN_1000KBPS 18
+#define CAN_3K3BPS   1
+#define CAN_5KBPS    2
+#define CAN_10KBPS   3
+#define CAN_20KBPS   4
+#define CAN_25KBPS   5 
+#define CAN_40KBPS   6
+#define CAN_50KBPS   7
+#define CAN_80KBPS   8
+#define CAN_100KBPS  9
+#define CAN_125KBPS  10
+#define CAN_200KBPS  11
+#define CAN_250KBPS  12
+#define CAN_500KBPS  13
+#define CAN_1000KBPS 14
 
 /* Common bit definition */
 #define BIT0             (1L)
